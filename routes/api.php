150e09b3816e172d393cacb7229c1a9d43a07824
Fabio Ribeiro <fabiohkd@gmail.com>
2022-06-27 09:29:52 -0300

    Adicionado Aulas

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,10 @@
 
 use App\Http\Controllers\Api\{
 		CourseController,
+    LessonController,
     ModuleController
 };
+
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,6 +23,9 @@ Route::get('/courses', [CourseController::class, 'index']);
 Route::get('/courses/{id}', [CourseController::class, 'show']);
 
 Route::get('/courses/{id}/modules', [ModuleController::class, 'index']);
+
+Route::get('/modules/{id}/lessons', [LessonController::class, 'index']);
+Route::get('/lessons/{id}', [LessonController::class, 'show']);
 
 Route::get('/', function (){
 	return response()->json([
